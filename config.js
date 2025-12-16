4e118c108d48dbb283173f1f14831b2960afec85
Mike Cross <mcross162@gmail.com>
2025-12-16 07:18:53 -0700

    security patches

---
+++ config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBceVpzCfvVY6JnbMCaK1Ye0zsnY9LOyI8",
+  authDomain: "ae-pricing-calculator.firebaseapp.com",
+  projectId: "ae-pricing-calculator",
+  storageBucket: "ae-pricing-calculator.firebasestorage.app",
+  messagingSenderId: "25810838101",
+  appId: "1:25810838101:web:9379152608fe8b8f9e6adc",
+  measurementId: "G-LQ8N00DH40"
+};
